def13479511de0a1f105b0dff5de760ac2cabc9a
JeppeKjeldal <jeppekjeldal95@gmail.com>
2024-05-08 09:47:40 +0200

    ComponentPattern
    Med gameworld.instance fejl

---
+++ 2SemesterEksamen/2SemesterEksamen/ComponentPattern/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComponentPattern
+{
+    internal class Enemy : Component
+    {
+        public float speed = 1;
+
+        public Vector2 velocity = new Vector2(0, 1);
+        public Enemy(GameObject gameObject) : base(gameObject)
+        {
+        }
+
+        float timeSinceLastSwitch;
+        float changeTime = 1f;
+
+        public override void Update(GameTime gameTime)
+        {
+            
+        }
+
+        public override void OnCollisionEnter(Collider col)
+        {
+
+        }
+
+        //Istate<Enemy> currentState;
+
+        //public void ChangeState(Istate<Enemy> state)
+        //{
+        //    if (currentState != null)
+        //    {
+        //        currentState.Exit();
+        //    }
+        //    currentState = state;
+        //    currentState.Enter(this);
+        //}
+    }
+}
---
+++ 2SemesterEksamen/2SemesterEksamen/ComponentPattern/Player.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComponentPattern
+{
+    internal class Player : Component
+    {
+        private float speed;
+
+        public Player(GameObject gameObject) : base(gameObject)
+        {
+
+        }
+
+        public void Move(Vector2 velocity)
+        {
+            if (velocity != Vector2.Zero)
+            {
+                velocity.Normalize();
+            }
+
+            velocity *= speed;
+
+            GameObject.Transform.Translate(velocity * GameWorld.Instance.DeltaTime);
+
+
+            //if (velocity.X > 0)
+            //{
+            //    animator.PlayAnimation("Right");
+            //}
+            //else if (velocity.X < 0)
+            //{
+            //    animator.PlayAnimation("Left");
+            //}
+        }
+
+        public override void Awake()
+        {
+
+        }
+
+        public void MoveByAddition(Vector2 velocity)
+        {
+            GameObject.Transform.Position += velocity;
+        }
+        
+        public override void Start()
+        {
+            SpriteRenderer sr = GameObject.GetComponent<SpriteRenderer>() as SpriteRenderer;
+            sr.SetSprite("SPRITE");
+            GameObject.Transform.Position = new Vector2(GameWorld.Instance.Graphics.PreferredBackBufferWidth / 2, GameWorld.Instance.Graphics.PreferredBackBufferHeight - sr.Sprite.Height / 3);
+        }
+
+        public void Attack()
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+
+        public override void OnCollisionEnter(Collider col)
+        {
+            base.OnCollisionEnter(col);
+        }
+    }
+}
