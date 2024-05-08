26ff7316f31322c7908148d29d19bb2360715a0d
JeppeKjeldal <jeppekjeldal95@gmail.com>
2024-05-08 09:58:52 +0200

    CommandPattern

---
+++ 2SemesterEksamen/2SemesterEksamen/CommandPattern/MoveCommand.cs
@@ -0,0 +1,33 @@
+﻿using ComponentPattern;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern
+{
+    internal class MoveCommand : ICommand
+    {
+        private Player player;
+        private Vector2 velocity;
+
+        public MoveCommand(Player player, Vector2 velocity)
+        {
+            this.player = player;
+            this.velocity = velocity;
+        }
+
+
+        public void Execute()
+        {
+            player.Move(velocity);
+        }
+
+        public void Undo()
+        {
+
+        }
+    }
+}
