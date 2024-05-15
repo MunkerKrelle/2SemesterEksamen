6b76c01f53134b18120954cace58fdb5e17290d3
Kristian Asbjørn <krasnielsen@gmail.com>
2024-05-15 09:29:19 +0200

    Update Inventory.cs

--- 2SemesterEksamen/2SemesterEksamen/ComponentPattern/Inventory.cs
+++ 2SemesterEksamen/2SemesterEksamen/ComponentPattern/Inventory.cs
@@ -5,6 +5,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
 using RepositoryPattern;
+using SharpDX.Win32;
 
 namespace ComponentPattern
 {
@@ -38,12 +39,17 @@ namespace ComponentPattern
 
         public void SellItem(Weapon weapon) { }
 
+
+        public void LoadItems()
+        {
+            var itemValues = database.ReturnValues(gameInventory[1]);
+        }
         public override void Draw(SpriteBatch spriteBatch)
         {
             SpriteFont font = GameWorld.Instance.Content.Load<SpriteFont>("text2");
             for (int i = 0; i < gameInventory.Count; i++) 
             {
-                var wrenchValues = database.ReturnValues(dicInventory[]);
+                var wrenchValues = database.ReturnValues(gameInventory[i]);
                 spriteBatch.DrawString(font, $"{wrenchValues[0].Item1}", new Vector2(450 + 50 * i, 250), Color.White);
                 spriteBatch.DrawString(font, $"{wrenchValues[0].Item2}", new Vector2(450 + 50 * i, 300), Color.White);
                 spriteBatch.DrawString(font, $"{wrenchValues[0].Item3}", new Vector2(450 + 50 * i, 350), Color.White);
