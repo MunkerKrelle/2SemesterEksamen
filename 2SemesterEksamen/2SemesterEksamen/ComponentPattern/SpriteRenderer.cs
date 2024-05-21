fe1d686977ffdda8c97f78c07ee31f549278b728
Kristian Asbjørn <krasnielsen@gmail.com>
2024-05-21 12:04:24 +0200

    notMain

--- 2SemesterEksamen/2SemesterEksamen/ComponentPattern/SpriteRenderer.cs
+++ 2SemesterEksamen/2SemesterEksamen/ComponentPattern/SpriteRenderer.cs
@@ -14,23 +14,19 @@ namespace ComponentPattern
 
         public Vector2 Origin { get; set; }
 
-        private float setLayer;
-
         public override void Start()
         {
             Origin = new Vector2(Sprite.Width / 2, Sprite.Height / 2);
         }
 
-        public void SetSprite(string spriteName, float layer)
+        public void SetSprite(string spriteName)
         {
             Sprite = GameWorld.Instance.Content.Load<Texture2D>(spriteName);
-            setLayer = layer;
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(Sprite, GameObject.Transform.Position, null, GameObject.Transform.Color, GameObject.Transform.Rotation, Origin, GameObject.Transform.Scale, SpriteEffects.None, 1);
-            spriteBatch.Draw(Sprite, GameObject.Transform.Position, null, Color.White, GameObject.Transform.Rotation, Origin, GameObject.Transform.Scale, SpriteEffects.None, setLayer);
+            spriteBatch.Draw(Sprite, GameObject.Transform.Position, null, GameObject.Transform.Color, GameObject.Transform.Rotation, Origin, GameObject.Transform.Scale, SpriteEffects.None, GameObject.Transform.Layer);
         }
     }
 }
