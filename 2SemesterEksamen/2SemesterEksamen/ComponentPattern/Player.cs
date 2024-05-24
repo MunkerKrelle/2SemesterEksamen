de9a4fe330fc347465bf6af2d7190b6386611c02
O-Verno <oliver.nvj@icloud.com>
2024-05-24 12:12:15 +0200

    doku

--- 2SemesterEksamen/2SemesterEksamen/ComponentPattern/Player.cs
+++ 2SemesterEksamen/2SemesterEksamen/ComponentPattern/Player.cs
@@ -11,6 +11,9 @@ using System.Text;
 
 namespace ComponentPattern
 {
+    /// <summary>
+    /// Repræsenterer spillerkomponenten, der styrer spillerens bevægelser, animationer og interaktioner.
+    /// </summary>
     class Player : Component
     {
         private float speed;
@@ -19,6 +22,10 @@ namespace ComponentPattern
         bool isAlive = true;
         public Animator animator;
         public Inventory inventory;
+
+        /// <summary>
+        /// Får eller sætter spillerens sundhed. Hvis sundheden når 0 eller derunder, vil spilleren dø.
+        /// </summary>
         public int Health
         {
             get { return health; }
@@ -33,6 +40,10 @@ namespace ComponentPattern
             }
         }
 
+        /// <summary>
+        /// Initialiserer en ny instans af <see cref="Player"/> klassen med det specificerede game object.
+        /// </summary>
+        /// <param name="gameObject">Det game object, som spilleren er knyttet til.</param>
         public Player(GameObject gameObject) : base(gameObject)
         {
 
@@ -40,6 +51,10 @@ namespace ComponentPattern
 
         bool isMoving;
 
+        /// <summary>
+        /// Flytter spilleren i henhold til den angivne hastighedsvektor.
+        /// </summary>
+        /// <param name="velocity">Hastighedsvektoren, som spilleren skal flytte sig med.</param>
         public void Move(Vector2 velocity)
         {
             if (velocity != Vector2.Zero)
@@ -62,6 +77,9 @@ namespace ComponentPattern
             }
         }
 
+        /// <summary>
+        /// Initialiserer spillerens egenskaber, når komponenten vågner.
+        /// </summary>
         public override void Awake()
         {
             speed = 100;
@@ -74,11 +92,18 @@ namespace ComponentPattern
             inventory.weaponsList[0].GameObject.Transform.Position = GameObject.Transform.Position;
         }
 
+        /// <summary>
+        /// Flytter spilleren ved at tilføje en vektor til spillerens nuværende position.
+        /// </summary>
+        /// <param name="velocity">Vektoren, som skal tilføjes til spillerens position.</param>
         public void MoveByAddition(Vector2 velocity)
         {
             GameObject.Transform.Position += velocity;
         }
 
+        /// <summary>
+        /// Initialiserer spillerens startposition og grafiske elementer.
+        /// </summary>
         public override void Start()
         {
             GameObject.Transform.Position = new Vector2(300, 300);
@@ -87,11 +112,18 @@ namespace ComponentPattern
             GameObject.Transform.Layer = 0.9f;
         }
 
+        /// <summary>
+        /// Opdaterer spillerens tilstand i hver frame.
+        /// </summary>
+        /// <param name="gameTime">Spillets tid, der er gået siden sidste opdatering.</param>
         public override void Update(GameTime gameTime)
         {
             inventory.weaponsList[0].GameObject.Transform.Position = GameObject.Transform.Position;
         }
 
+        /// <summary>
+        /// Udfører spillerens angrebsanimation.
+        /// </summary>
         public void Attack()
         {
             Inventory inventory = GameObject.GetComponent<Inventory>() as Inventory;
@@ -102,6 +134,10 @@ namespace ComponentPattern
 
         }
 
+        /// <summary>
+        /// Håndterer kollisioner med andre objekter.
+        /// </summary>
+        /// <param name="col">Kollisionen, som spilleren er involveret i.</param>
         public override void OnCollisionEnter(Collider col)
         {
             Enemy enemy = (Enemy)col.GameObject.GetComponent<Enemy>();
@@ -114,18 +150,26 @@ namespace ComponentPattern
             base.OnCollisionEnter(col);
         }
 
+        /// <summary>
+        /// Udfører angrebet på fjender (placeholder-metode).
+        /// </summary>
         private void AttackEnemy()
         {
 
         }
 
+        /// <summary>
+        /// Håndterer spillerens død ved at vise en respawn-knap og destruere spillerens game object.
+        /// </summary>
         private void Die()
         {
-            Debug.WriteLine("debug");
-            
-            GameWorld.Instance.CreateRespawnButton();GameWorld.Instance.Destroy(GameObject);
+            GameWorld.Instance.CreateRespawnButton(); 
+            GameWorld.Instance.Destroy(GameObject);  
         }
 
+        /// <summary>
+        /// Genopliver spilleren ved at genoprette helbred og position.
+        /// </summary>
         public void Respawn()
         {
             Health = 100;
