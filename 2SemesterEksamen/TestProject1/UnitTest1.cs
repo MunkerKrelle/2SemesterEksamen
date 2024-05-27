8c4fa17210beacea292775948eed1f59c5d4d12e
Kristian Asbjørn <krasnielsen@gmail.com>
2024-05-27 11:21:22 +0200

    Unit Test

---
+++ 2SemesterEksamen/TestProject1/UnitTest1.cs
@@ -0,0 +1,13 @@
+using _2SemesterEksamen;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
